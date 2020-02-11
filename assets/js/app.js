854c4c4076ee79dda31e6dcdb4fa5be60705854a
Noah Mistry <noah.mistry@gmail.com>
2020-02-10 17:59:36 -0800

    V.1 Project Files

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+// Initialize Flickity Carousel //
+var flkty = new Flickity('.main-carousel',{
+// options //
+draggable: false,
+pageDots: false,
+prevNextButtons: false,
+contain: true,
+});
---
+++ gulpfile.js
@@ -0,0 +1,146 @@
+(() => {
+
+    'use strict';
+  
+    /**************** Gulp.js 4 configuration ****************/
+  
+    const
+  
+      // development or production
+      devBuild  = ((process.env.NODE_ENV || 'development').trim().toLowerCase() === 'development'),
+  
+      // directory locations
+      dir = {
+        src         : 'src/build/',
+        build       : 'assets/'
+      },
+  
+      // modules
+      gulp          = require('gulp'),
+      del           = require('del'),
+      noop          = require('gulp-noop'),
+      newer         = require('gulp-newer'),
+      size          = require('gulp-size'),
+      imagemin      = require('gulp-imagemin'),
+      sass          = require('gulp-sass'), // or use gulp-dart-sass
+      postcss       = require('gulp-postcss'),
+      sourcemaps    = devBuild ? require('gulp-sourcemaps') : null,
+      browsersync   = devBuild ? require('browser-sync').create() : null;
+  
+  
+    console.log('Gulp', devBuild ? 'development' : 'production', 'build');
+  
+  
+    /**************** clean task ****************/
+  
+    function clean() {
+  
+      return del([ dir.build ]);
+  
+    }
+    exports.clean = clean;
+    exports.wipe = clean;
+  
+  
+    /**************** images task ****************/
+  
+    const imgConfig = {
+      src           : dir.src + 'images/**/*',
+      build         : dir.build + 'images/',
+      minOpts: {
+        optimizationLevel: 5
+      }
+    };
+  
+    function images() {
+  
+      return gulp.src(imgConfig.src)
+        .pipe(newer(imgConfig.build))
+        .pipe(imagemin(imgConfig.minOpts))
+        .pipe(size({ showFiles:true }))
+        .pipe(gulp.dest(imgConfig.build));
+  
+    }
+    exports.images = images;
+  
+  
+    /**************** CSS task ****************/
+  
+    const cssConfig = {
+  
+      src         : dir.src + 'scss/styles.scss',
+      watch       : dir.src + 'scss/**/*',
+      build       : dir.build + 'css/',
+      sassOpts: {
+        sourceMap       : devBuild,
+        imagePath       : '/images/',
+        precision       : 3,
+        errLogToConsole : true
+      },
+  
+      postCSS: [
+        require('usedcss')({
+          html: ['index.html']
+        }),
+        require('postcss-assets')({
+          loadPaths: ['images/'],
+          basePath: dir.build
+        }),
+        require('cssnano')
+      ]
+  
+    };
+  
+  
+    function css() {
+  
+      return gulp.src(cssConfig.src)
+        .pipe(sourcemaps ? sourcemaps.init() : noop())
+        .pipe(sass(cssConfig.sassOpts).on('error', sass.logError))
+        .pipe(postcss(cssConfig.postCSS))
+        .pipe(sourcemaps ? sourcemaps.write() : noop())
+        .pipe(size({ showFiles: true }))
+        .pipe(gulp.dest(cssConfig.build))
+        .pipe(browsersync ? browsersync.reload({ stream: true }) : noop());
+  
+    }
+    exports.css = gulp.series(images, css);
+  
+  
+    /**************** server task (private) ****************/
+  
+    const syncConfig = {
+      server: {
+        baseDir   : './',
+      },
+      files: './index.html',
+      port        : 5500,
+      open        : false,
+    };
+  
+    // browser-sync
+    function server(done) {
+      if (browsersync) browsersync.init(syncConfig);
+      done();
+    }
+  
+  
+    /**************** watch task (private) ****************/
+  
+    function watch(done) {
+  
+      // image changes
+      gulp.watch(imgConfig.src, images);
+  
+      // CSS changes
+      gulp.watch(cssConfig.watch, css);
+  
+      done();
+  
+    }
+  
+    /**************** default task ****************/
+  
+    exports.default = gulp.series(exports.css, watch, server);
+  
+  })();
